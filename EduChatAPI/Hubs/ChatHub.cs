61850d6c9706e023709ef99cfc23146869c1b530
Tom Knighton <tom@Toms-MacBook-Air.local>
2019-09-08 16:11:37 +0100

    Group events

--- EduChatAPI/Hubs/ChatHub.cs
+++ EduChatAPI/Hubs/ChatHub.cs
@@ -6,10 +6,24 @@ namespace EduChatAPI.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendTestMessage(string user, string message)
+
+        public async Task SendMessage(int senderid, string groupToSendTo, int messageId)
         {
-            await Clients.All.SendAsync("RecieveTestMessage", user, message);
-            //Sends the message back to all connected clients
+            // Client calls SendMessage with senderid, the id of the group to send to and the id of the message
+            await Clients.Group(groupToSendTo).SendAsync("MessageRecieved", senderid, messageId);
+            //All clients in that group listen for the MessageRecieved event with the senderid and the messageid
         }
+
+
+
+        public async Task SubscribeToGroup(string groupId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
+        }
+        public async Task RemoveFromGroup(string groupId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupId);
+        }
+       
     }
 }
