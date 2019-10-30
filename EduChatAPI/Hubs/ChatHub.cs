711785fedcfae726c343210892708fe4593d1ee4
Tom Knighton <tom@Toms-MacBook-Air.local>
2019-10-30 20:34:00 +0000

    test

--- EduChatAPI/Hubs/ChatHub.cs
+++ EduChatAPI/Hubs/ChatHub.cs
@@ -7,25 +7,20 @@ namespace EduChatAPI.Hubs
     public class ChatHub : Hub
     {
 
-        public async Task SendChatMessage(int senderid, string groupToSendTo, int messageId)
+        public async Task SendMessage(int senderid, string groupToSendTo, int messageId)
         {
-            await Clients.All.SendAsync("BROADCAST", "RECIEVED");
             // Client calls SendMessage with senderid, the id of the group to send to and the id of the message
-            //await Clients.Group(groupToSendTo).SendAsync("ChatMessageRecieved", groupToSendTo, senderid, messageId);
+            await Clients.Group(groupToSendTo).SendAsync("MessageRecieved", groupToSendTo, senderid, messageId);
             //All clients in that group listen for the MessageRecieved event with the senderid and the messageid
         }
 
-        public async Task CreateChatMessage(string msg)
-        {
-            await Clients.All.SendAsync("BROADCAST", msg);
-        }
+       
         
 
         public async Task SubscribeToGroup(string groupId)
         {
             //Temporarily adds client to group subscription
             await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
-            await Clients.All.SendAsync("BROADCAST", "SUBJECT SUBSCRIBED");
         }
         public async Task RemoveFromGroup(string groupId)
         {
