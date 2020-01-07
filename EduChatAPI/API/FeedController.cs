a6868f3d7222119c304a0d4b198d8f086c914f6f
Tom Knighton <tom@Toms-MacBook-Air.local>
2020-01-07 20:08:54 +0000

    Added endpoints

--- EduChatAPI/API/FeedController.cs
+++ EduChatAPI/API/FeedController.cs
@@ -22,5 +22,10 @@ namespace EduChatAPI.API
         {
             return Ok(await new FeedTasks().GetAllPostsForSubject(subjectid));
         }
+        [HttpPost("SetLikeForPost/{postid}/{userid}/{like}")]
+        public async Task<IActionResult> SetLikeForPost(int postid, int userid, bool like)
+        {
+            return Ok(await new FeedTasks().SetLikeStatus(postid, userid, like));
+        }
     }
 }
